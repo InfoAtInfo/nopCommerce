fac1e34efaff08cf6a922f887d9cb7dad4c6221b
InfoAtInfo <info@ena.rs>
2023-02-21 16:36:44 +0100

    Added redirect plugin

---
+++ src/Presentation/Nop.Web/Plugins/FoxNetSoft.RedirectManager/Install/SqlServer.RedirectManager.sql
@@ -0,0 +1,68 @@
+﻿IF EXISTS (SELECT 1 FROM sys.columns WHERE object_id=object_id('[FNS_UrlRewriteRecord]'))
+begin
+	IF not EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[FNS_UrlRewriteRecord]') AND name = N'IX_FNS_UrlRewriteRecord_RequestUrl')
+	begin
+		CREATE NONCLUSTERED INDEX IX_FNS_UrlRewriteRecord_RequestUrl ON FNS_UrlRewriteRecord ([RequestUrl],[StoreId]) 
+	end
+	IF not EXISTS (SELECT * FROM sys.indexes WHERE object_id = OBJECT_ID(N'[FNS_Url404Error]') AND name = N'IX_FNS_Url404Error_Url')
+	begin
+		CREATE NONCLUSTERED INDEX IX_FNS_Url404Error_Url ON FNS_Url404Error ([Url]) 
+	end
+END
+GO
+IF  EXISTS (SELECT * FROM sys.objects WHERE object_id = OBJECT_ID(N'[FNS_RedirectManager_GetUrlRewriteRecordsByRequestUrl]') AND type in (N'P', N'PC'))
+DROP PROCEDURE [FNS_RedirectManager_GetUrlRewriteRecordsByRequestUrl]
+GO
+CREATE PROCEDURE [FNS_RedirectManager_GetUrlRewriteRecordsByRequestUrl]
+(
+	@requestUrl nvarchar(max),	--requestUrl
+	@storeId	int, -- Store Id
+	@fulllMatch bit  -- If look for the full match
+)
+AS
+BEGIN
+SET NOCOUNT ON
+declare @id int
+set @id=0 
+if @fulllMatch=0
+	select top 1 @id = F.Id
+	from FNS_UrlRewriteRecord F WITH(NOLOCK) 
+	where F.RequestUrl = @requestUrl and (F.StoreId=@storeId or F.StoreId=0) 
+	order by F.StoreId desc
+else
+	select top 1 @id = F.Id
+	from FNS_UrlRewriteRecord F WITH(NOLOCK) 
+	where F.RequestUrl = @requestUrl and F.StoreId=@storeId
+	order by F.StoreId desc
+
+if @fulllMatch=0 and @id=0
+begin
+	if charindex('?', @requestUrl)>0
+	begin
+		declare @newRequestUrl nvarchar(max)
+		set @newRequestUrl = substring(@requestUrl, 0, charindex('?', @requestUrl))
+		if len(@newRequestUrl)>0 and right(@newRequestUrl,1)='/'
+		begin
+			set @newRequestUrl = substring(@newRequestUrl, 0, len(@newRequestUrl))
+		end
+		select top 1 @id = F.Id
+		from FNS_UrlRewriteRecord F WITH(NOLOCK) 
+		where F.RequestUrl = @newRequestUrl
+			and (F.StoreId=@storeId or F.StoreId=0) 
+		order by F.StoreId desc
+	END
+	/*
+	if @id=0
+	begin
+		select top 1 @Id = F.Id 
+		from FNS_UrlRewriteRecord F WITH(NOLOCK, INDEX(IX_FNS_UrlRewriteRecord_RequestUrl)) 
+		where F.RequestUrl like '%*' 
+			and @requestUrl like substring(F.RequestUrl, 0, charindex('*', F.RequestUrl))+'%'
+			and (F.StoreId=@storeId or F.StoreId=0) 
+		order by F.StoreId desc
+	END*/
+end
+
+select * from FNS_UrlRewriteRecord F WITH(NOLOCK) where F.Id=@id
+END
+GO
---
+++ src/Presentation/Nop.Web/Plugins/FoxNetSoft.RedirectManager/Install/Update105.RedirectManager.sql
@@ -0,0 +1,21 @@
+﻿IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id=object_id('[FNS_UrlRewriteRecord]') and NAME='Comment')
+BEGIN
+	ALTER TABLE [FNS_UrlRewriteRecord]
+	ADD [Comment] nvarchar(100) NULL
+END
+GO
+update [FNS_UrlRewriteRecord]
+set RequestUrl = SUBSTRING(RequestUrl,1,len(RequestUrl)-1)
+where len(RequestUrl)>0 and SUBSTRING(RequestUrl,len(RequestUrl),1)='/'
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id=object_id('[FNS_Url404Error]') and NAME='Count')
+BEGIN
+	ALTER TABLE [FNS_Url404Error] ADD [Count] int NOT NULL CONSTRAINT DF_FNS_Url404Error_Count DEFAULT 1
+END
+GO
+IF NOT EXISTS (SELECT 1 FROM sys.columns WHERE object_id=object_id('[FNS_UrlRewriteRecord]') and NAME='PreserveQueryStrings')
+BEGIN
+	ALTER TABLE [FNS_UrlRewriteRecord]
+	ADD [PreserveQueryStrings] bit NOT NULL CONSTRAINT DF_FNS_UrlRewriteRecord_PreserveQueryStrings DEFAULT 0
+END
+GO
